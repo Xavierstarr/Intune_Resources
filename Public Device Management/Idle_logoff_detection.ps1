dd0ff56317335ffa93d2689c427ee0d3565c2c48
Xavierstarr <118952632+Xavierstarr@users.noreply.github.com>
2023-09-18 11:01:00 +0930

    Add files via upload

---
+++ Public Device Management/Idle_logoff_detection.ps1
@@ -0,0 +1,11 @@
+﻿$taskName = "Idle-Logoff"
+$taskStatus = get-scheduledtask | Where-object {$_.taskName -eq "$taskName"}
+
+if ($taskStatus){
+    Write-Host "Task exists, no action is required"
+    Exit 0
+}
+Else{
+    Write-Host "Task not found, remediation is required"
+    Exit 1
+}
---
+++ Public Device Management/Idle_logoff_task.ps1
@@ -0,0 +1,40 @@
+﻿# This script should will create a scheduled task that will begin at the time of running, then repeat every 30 Minutes.
+# At trigger the task will be checking if Idle state has been hit, so if user has been inactive for $IdleDuration in minutes
+
+# Set Idle Duation Period
+$IdleDuration = 30
+$Timespan = "00:$(($IdleDuration).ToString().PadLeft(2, '0')):00"
+$hostname = Hostname
+
+#Custom Task Path & Name, where other public tasks will also be contained for better house keeping
+$TaskPath = "\Public Device Management\"
+$taskName = "Idle-Logoff"
+
+$taskstatus = get-scheduledtask | Where-object {$_.taskName -eq "$taskName"}
+
+if (!$taskstatus){
+    try{
+        Write-Host "Idle Logoff task does not Exists. Creating Task."
+        
+        #Task Variables
+        $STaction  = New-ScheduledTaskAction -Execute 'c:\windows\system32\shutdown.exe' -Argument '-r -t 0'
+        $STTrigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes $IdleDuration) -RepetitionDuration (New-TimeSpan -Days (365 * 20))
+        $STSet     = New-ScheduledTaskSettingsSet -RunOnlyIfIdle -IdleDuration $Timespan -AllowStartIfOnBatteries -WakeToRun
+        $STuser    = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+        
+        #Register new task with above varibles
+        Register-ScheduledTask -TaskName "$taskName" -TaskPath "$TaskPath"  -Action $STaction -Settings $STSet -Trigger $STtrigger -Principal $STuser
+        Exit 0
+    }
+
+    Catch {
+            Write-Host "Error in Creating scheduled task"
+            Write-error $_
+            Exit 1
+
+    }
+}
+Else{
+        Write-Host "Task exists, no action is required"
+        Exit 0
+}
